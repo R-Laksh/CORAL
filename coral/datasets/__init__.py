4e4e2e11d3c07106f9ac12f4d12c4e6e9c306960
laksherthanlife <102166996+laksherthanlife@users.noreply.github.com>
2026-09-06 12:11:46 +0800

    Export GB1 dataset utilities lazily

--- coral/datasets/__init__.py
+++ coral/datasets/__init__.py
@@ -7,6 +7,10 @@ _EXPORTS = {
     "decode_idx": ".seqgra_dataset", "decode_idx_batch": ".seqgra_dataset",
     "ALPH": ".seqgra_dataset", "ALPH_DECODE": ".seqgra_dataset",
     "load_orientation_neighbourhoods": ".tfbs_mpra",
+    "AA_ORDER": ".gb1", "EDIT_POSITIONS": ".gb1", "WT_FULL_SEQUENCE": ".gb1",
+    "WT_GENOTYPE": ".gb1", "genotype_from_ids": ".gb1", "genotype_hamming": ".gb1",
+    "genotype_to_full_sequence": ".gb1", "hamming_shell": ".gb1",
+    "load_gb1_processed": ".gb1", "sequence_to_ids": ".gb1",
 }
 __all__ = list(_EXPORTS)
 
