5a8baa74f543b0d03911add149505748480de15f
laksherthanlife <102166996+laksherthanlife@users.noreply.github.com>
2026-09-06 11:56:36 +0800

    Export biological predictor adapters

--- coral/models/__init__.py
+++ coral/models/__init__.py
@@ -1,2 +1,15 @@
 from .gc_model import GenomicGCModel, train_head
 from .seqgra_models import CNN1D, DeepSTARR
+from .biological import (
+    BPNetCountScore,
+    ConjunctiveConstraint,
+    ESMAlphabet,
+    ESMSoftSequenceRegressor,
+    ThresholdConstraint,
+)
+
+__all__ = [
+    "GenomicGCModel", "train_head", "CNN1D", "DeepSTARR",
+    "BPNetCountScore", "ConjunctiveConstraint", "ESMAlphabet",
+    "ESMSoftSequenceRegressor", "ThresholdConstraint",
+]
